92bc43b02f3737320a2135d64b7704405ff903ce
Clement MARCEL <lecramproject@gmail.com>
2024-10-01 18:38:58 +0200

    add load dotenv, drf_yasg

--- app/settings.py
+++ app/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 import os
 from pathlib import Path
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,9 +40,11 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "authentication",
     "event",
     "blog",
-    "contact"
+    "contact",
+    "drf_yasg"
 ]
 
 MIDDLEWARE = [
