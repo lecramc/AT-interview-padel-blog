fe1e66c214ec4adec31a30504a9618a19c898806
Clement MARCEL <lecramproject@gmail.com>
2024-10-04 11:54:22 +0200

    remove register feature

--- authentication/urls.py
+++ authentication/urls.py
@@ -2,10 +2,9 @@ from django.contrib.auth import views as auth
 
 from django.urls import path
 
-from authentication.views import register_view, logout_view
+from authentication.views import logout_view
 
 urlpatterns = [
     path("login/", auth.LoginView.as_view(template_name="login.html"), name="login"),
     path("logout/", logout_view, name="logout"),
-    path('register/', register_view, name='register'),
 ]
--- authentication/views.py
+++ authentication/views.py
@@ -9,18 +9,6 @@ from authentication.forms import RegisterForm
 def login_view(request):
     return render(request, 'login.html')
 
-
-def register_view(request):
-    if request.method == 'POST':
-        form = RegisterForm(request.POST)
-        if form.is_valid():
-            user = form.save()
-            return redirect('login')
-    else:
-        form = RegisterForm()
-
-    return render(request, 'register.html', {'form': form})
-
 def logout_view(request):
     logout(request)
     return redirect("login")
